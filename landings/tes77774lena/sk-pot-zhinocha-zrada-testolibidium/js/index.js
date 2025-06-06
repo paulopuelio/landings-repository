4e6ac697e68ea06475cfc9047d015c55a91ad950
Landing Manager <admin@lp.com>
2025-06-06 13:26:29 +0200

    Deploy landing: tes77774lena

---
+++ landings/tes77774lena/sk-pot-zhinocha-zrada-testolibidium/js/index.js
@@ -0,0 +1,90 @@
+const months=['január','február','március','április','május','június','július','augusztus','szeptember','október','november','december'],monthMin = ['','','','','','','','','','','',''],days = ['vasárnap','hétfő','kedd','szerda','csütörtök','péntek','szombat'],daysMin = ['','','','','','',''],seasons = ['tél','tavasz','nyár','ősz'];function postDate(daysName, daysMinName, monthsName, monthsMinName, seasonsName) {const _counterLength = 60;for (let counter = 0; counter < _counterLength; counter++) {innerDate(counter, 'date-');innerDate(counter, 'date')} function innerDate(counter, dateType) {let newCounter;dateType === 'date-' ? newCounter = -counter : newCounter = counter; const _msInDay = 86400000, _localDate = new Date(Date.now() + (newCounter * _msInDay)), _day = _localDate.getDate(), _month = _localDate.getMonth() + 1, _year = _localDate.getFullYear(); const dayDefault = addZero(_day), monthDefault = addZero(_month), defaultDate = dayDefault + '.' + monthDefault + '.' + _year; const dateClass = dateType + counter, nodeList = document.querySelectorAll('.' + dateClass); for (let i = 0; i < nodeList.length; i++) {const dateFormat = nodeList[i].dataset.format;dateFormat !== undefined && dateFormat !== ''? nodeList[i].innerHTML = String(changeFormat(dayDefault, _month, _year, dateFormat, newCounter)): nodeList[i].innerHTML = defaultDate} } function changeFormat(_day, _month, _year, format, counter) { let innerFormat = format; const testFormat = ["dd","mm","yyyy","monthFull","monthOnly","year"], dateFormat = { dd: _day, mm: addZero(_month), yyyy: _year, monthFull: getMonthName(_month, monthsName, false), monthOnly: getMonthName(_month, monthsName, false, counter), year: getYearWithCounter(_year, counter), }; for (let i = 0; i < testFormat.length; i++) { let string = testFormat[i]; let regExp = new RegExp(string); innerFormat = innerFormat.replace(regExp, dateFormat[string]); } return innerFormat.split(' ').join(' ') } function getMonthName(_month, monthsName, bigFirstLetter, counter) { const monthCounter = !!counter ? counter : 0; let month; _month + monthCounter > 12 ? month = monthCounter - (12 - _month) : month = _month + monthCounter; _month + monthCounter <= 0 ? month = 12 + monthCounter + 1 : month = _month + monthCounter; return changeFirstLetter(bigFirstLetter, monthsName[month - 1]) } function getYearWithCounter(year, counter) {return year + counter} function addZero(numb){return numb<10?'0'+numb:numb} function changeFirstLetter(isBig,str){return isBig&&str&&str.length>0?str[0].toUpperCase()+str.slice(1):str} }if (document.body.classList.contains('ev-date')) {document.addEventListener("DOMContentLoaded", function () {postDate(days, daysMin, months, monthMin, seasons)});}
+$(document).ready(function () {
+
+
+  $('.boxes > div').click(function () {
+    if ($(this).parent().hasClass('boxesfirsttry')) {
+      $('.boxes').removeClass('boxesfirsttry');
+      $(this).addClass('openbox');
+      $(this).find('.try').hide();
+      $(this).find('.opentry').show();
+      setTimeout(function () {
+        $('.sweet-overlay').show();
+        $('.sweet-alert').show();
+      }, 500);
+    } else if ($(this).parent().hasClass('boxeslasttry')) {
+      if (!$(this).hasClass('openbox')) {
+        $(this).find('.try').hide();
+        $(this).find('.opentry').show();
+        $(this).find('.boxtext').css('display', 'block');
+        setTimeout(function () {
+          $('.spin-result-wrapper').show();
+          setTimeout(function () {
+            $('#boxesContainer').slideUp(250);
+            setTimeout(function () {
+              $('.order_block').slideDown(250);
+            }, 500);
+          }, 500);
+        }, 500);
+      }
+    }
+  });
+
+  $('#update').click(function () {
+    $('.sweet-overlay').hide();
+    $('.sweet-alert').hide();
+    $('.boxes').addClass('boxeslasttry');
+  });
+
+  $('.pop-up-button, .close-popup').click(function () {
+    $('.spin-result-wrapper').hide();
+    $(this).attr('href', '#form');
+    start_timer();
+    
+  });
+});
+
+
+var linkNav = document.querySelectorAll('[href^="#"]'),
+V = 0.05;
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1');
+        t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+        requestAnimationFrame(step);
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+            r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step);
+            } else {
+                location.hash = hash;
+            }
+        }
+    }, false);
+}
+
+var time = 600;
+var intr;
+      
+function start_timer() {
+  intr = setInterval(tick, 1000);
+}
+
+function tick() {
+  time = time - 1;
+  var mins = Math.floor(time / 60);
+  var secs = time - mins * 60;
+  if (mins == 0 && secs == 0) {
+    clearInterval(intr);
+  }
+  secs = secs >= 10 ? secs : '0' + secs;
+  $('#min').html('0' + mins);
+  $('#sec').html(secs);
+}
+
